ebd44ea6e95e4b32d5999c38025896ae6342a925
陈晓佳 <chenxj@bjrrtx.com>
2019-01-31 16:59:51 +0800

    fix:java基础语法代码Model

---
+++ SyntaxBase/src/DataType/HelloGithub.java
@@ -0,0 +1,12 @@
+package DataType;
+
+/**
+ * ClassName: <br>
+ * Description: <br>
+ * Company: rrtx
+ *
+ * @author chenxj
+ * @version v1.0.0    2019/1/31 16:58 chenxj  文件初始创建
+ */
+public class HelloGithub {
+}
